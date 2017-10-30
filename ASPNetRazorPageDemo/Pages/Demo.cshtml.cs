7808c6e21d8700956bf420f91801192cf9370b8d
talkingdotnet <talkingdotnet@gmail.com>
2017-10-30 20:17:15 +0530

    Add project files.

---
+++ ASPNetRazorPageDemo/Pages/Demo.cshtml.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ASPNetRazorPageDemo.Pages
+{
+    public class DemoModel : PageModel
+    {
+        public string Message { get; set; }
+        public List<string> MessageList { get; set; }
+
+        public void OnGet()
+        {
+            Message = "On Demo Page";
+        }
+
+        public JsonResult OnGetList()
+        {
+            List<string> lstString = new List<string>
+            {
+                "Val 1",
+                "Val 2",
+                "Val 3"
+            };
+            return new JsonResult(lstString);
+        }
+
+        public ActionResult OnPostSend()
+        {
+            string sPostValue1 = "";
+            string sPostValue2 = "";
+            string sPostValue3 = "";
+            {
+                MemoryStream stream = new MemoryStream();
+                Request.Body.CopyTo(stream);
+                stream.Position = 0;
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string requestBody = reader.ReadToEnd();
+                    if(requestBody.Length > 0)
+                    {
+                        var obj = JsonConvert.DeserializeObject<PostData>(requestBody);
+                        if(obj != null)
+                        {
+                            sPostValue1 = obj.Item1;
+                            sPostValue2 = obj.Item2;
+                            sPostValue3 = obj.Item3;
+                        }
+                    }
+                }
+            }
+            List<string> lstString = new List<string>
+            {
+                sPostValue1,
+                sPostValue2,
+                sPostValue3
+            };
+            return new JsonResult(lstString);
+        }
+    }
+
+    public class PostData
+    {
+        public string Item1 { get; set; }
+        public string Item2 { get; set; }
+        public string Item3 { get; set; }
+    }
+}
